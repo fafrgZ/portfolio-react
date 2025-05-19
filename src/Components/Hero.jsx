1e1dcdbd9d668fbd55d0e147facec90265ff5114
Farid <faridfragozo@gmail.com>
2025-05-19 17:38:03 -0300

    preparando para lanzar

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -60,7 +60,7 @@ const Hero = () => {
                     </motion.p>
                     <motion.a
                         variants={childVariants} 
-                        href="/resume.pdf"
+                        href="/CurriculumVitaeFragozoSturba.pdf"
                         target='_blank'
                         rel='noopener noreferrer'
                         download
